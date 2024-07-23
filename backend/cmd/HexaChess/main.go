8a3254eade398f7c33642b56b167597fec57ddf4
bingKegeta <sohamg24@gmail.com>
2024-07-23 17:32:00 -0400

    Open db connection on starting the server

--- backend/cmd/HexaChess/main.go
+++ backend/cmd/HexaChess/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-
+	db "github.com/BingKegeta/Hexachess/backend/DB"
 	"github.com/BingKegeta/Hexachess/backend/internal/api"
 )
 
+type App struct {
+	router http.Handler
+}
+
+func New() *App {
+	app := &App{
+		router: api.Routes(),
+	}
+	fmt.Println("Server started.")
+
+	return app
+}
+
+func (a *App) Start(ctx context.Context) error {
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: a.router,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("failed to start server: %w", err)
+	}
+	return nil
+}
+
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.DefaultLogger)
+	db.InitDB("../../DB/hexachess.db")
+	defer db.CloseDB()
 
-	r.Get("/board", api.GetBoard)
-
-	http.ListenAndServe(":8000", r)
+	app := New()
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start app:", err)
+	}
 }
